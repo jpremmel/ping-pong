3abf75f29f0df893548ef1e3f6d4477e7a22bd5a
Student <example@example.com>
2019-09-23 08:39:46 -0700

    add initial files. Store userInput in a variable.

---
+++ Ping-Pong.cs
@@ -0,0 +1,11 @@
+using System;
+
+class  PingPong
+{
+  static void Main()
+  {
+    Console.WriteLine("Please enter a number:");
+    int userInput = int.Parse(Console.ReadLine());
+    Console.WriteLine(userInput + 4);
+  }
+}
