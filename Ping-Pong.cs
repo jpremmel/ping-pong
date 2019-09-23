034b950f7d5a06829b019f566dc0d9eacf2afac4
Student <example@example.com>
2019-09-23 09:31:08 -0700

    Add else if condtions.

--- Ping-Pong.cs
+++ Ping-Pong.cs
@@ -1,14 +1,28 @@
 using System;
-
 class  PingPong
 {
   static void Main()
   {
     Console.WriteLine("Please enter a number:");
     int userInput = int.Parse(Console.ReadLine());
-    for (int i = 0; i <= userInput; i++)
+    for (int i = 1; i <= userInput; i++)
     {
-      Console.WriteLine(i);
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine("PING-PONG");
+      }
+      else if (i % 3 == 0)
+      {
+        Console.WriteLine("PING");
+      }
+      else if (i % 5 == 0)
+      {
+        Console.WriteLine("PONG");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
     }
   }
 }
